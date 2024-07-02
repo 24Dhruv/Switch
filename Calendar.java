32b86ad321ac0526cd82eecd21db50c159e7969a
Dhruv Khandelwal <137927657+24Dhruv@users.noreply.github.com>
2024-07-02 18:42:19 +0530

    Create Calendar.java

---
+++ Calendar.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class practprob {
+    public static void main(String args[]){
+           int calender =sc.nextInt();
+            switch (calender) {
+                case 1: 
+                    System.out.println("january");
+                    break;
+                case 2: 
+                    System.out.println("februry");
+                    break;
+                case 3: 
+                    System.out.println("march");
+                    break;
+                    case 4: 
+                    System.out.println("april");
+                    break;
+                    case 5: 
+                    System.out.println("may");
+                    break;
+                    case 6: 
+                    System.out.println("june");
+                    break;
+                    case 7: 
+                    System.out.println("july");
+                    break;
+                    case 8: 
+                    System.out.println("august");
+                    break;
+                    case 9: 
+                    System.out.println("sept");
+                    break;
+                    case 10: 
+                    System.out.println("oct");
+                    break;
+                    case 11: 
+                    System.out.println("nov");
+                    break;
+                    
+                default:
+                    break;
+            }   
+    }
+}
